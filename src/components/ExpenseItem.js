7d1240b1396ba813b942b9a7d358b3a243409a62
Adeel-4726 <adeelhussainsiddiqi@gmail.com>
2023-09-27 03:42:33 -0400

    Changes made by Adeel Idealist

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
+import { TiDelete, TiPlus, TiMinus, TiAdd, TiUserAdd } from 'react-icons/ti';
+import RiAddFill from 'react-icons/ri';
+
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
@@ -25,12 +27,27 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: -10,
+        };
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+
+    }
+
+
     return (
         <tr>
         <td>{props.name}</td>
         <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        <td><TiPlus color='green' onClick={event=> increaseAllocation(props.name)}>TiPlus</TiPlus></td>
+        <td><TiMinus onClick={event=> decreaseAllocation(props.name)}>TiMinus</TiMinus></td>
+        <td><TiDelete size='1.5em' color='red' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
 };
