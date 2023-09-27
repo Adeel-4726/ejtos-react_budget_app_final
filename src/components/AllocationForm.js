963ee7f15f447db7fe1bba5d3211739829a3514b
Adeel-4726 <adeelhussainsiddiqi@gmail.com>
2023-09-27 04:09:05 -0400

    Changes made by Adeel Idealist

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -58,13 +58,16 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
+                  
+                  <span style={{ paddingLeft: "10px", paddingTop: "3px", fontSize: "20px" }}>&nbsp;&nbsp;&nbsp;£</span>
+
 
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
+                        style={{ marginLeft: '0.2rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
 
